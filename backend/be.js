027a1e40da8d9bdb7be3838a80f7f7a5d5484dde
AnhP03 <63649337+duyanh131212@users.noreply.github.com>
2025-04-13 21:29:09 -0400

    remove filestore

--- backend/be.js
+++ backend/be.js
@@ -33,29 +33,6 @@ const firebaseConfig = {
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const clientAuth = getClientAuth(firebaseApp);
 
-// Get Firestore instance
-const db = getFirestore(firebaseApp);
-
-// Middleware to verify Firebase ID token
-const authenticateUser = async (req, res, next) => {
-  const authHeader = req.headers.authorization;
-  
-  if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    return res.status(401).json({ error: 'Unauthorized: No token provided' });
-  }
-
-  const idToken = authHeader.split('Bearer ')[1];
-  
-  try {
-    const decodedToken = await admin.auth().verifyIdToken(idToken);
-    req.user = decodedToken;
-    next();
-  } catch (error) {
-    console.error('Error verifying auth token:', error);
-    return res.status(401).json({ error: 'Unauthorized: Invalid token' });
-  }
-};
-
 // Authentication routes
 // Sign Up
 app.post('/auth/signup', async (req, res) => {
